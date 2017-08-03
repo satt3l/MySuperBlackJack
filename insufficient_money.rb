488eb60a725116e366da339180c4c812037d46b1
Evgeniy Fateev <spaceflow@gmail.com>
2017-08-03 14:36:15 +0300

    Add comments

--- insufficient_money.rb
+++ insufficient_money.rb
@@ -1,2 +1,4 @@
+# NOTE: обычно классы исключений заканчивают на `Error`, например: `NotEnoughMoneyError`.
+# NOTE: также можно классы исключений поместить в каталог `errors`.
 class InsufficientMoney < StandardError
 end
